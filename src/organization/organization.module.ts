1c5c73a1115ea0d0ce29fc2a65a89933e4373039
darian7 <yoj.cabrera@udla.edu.co>
2020-02-17 04:37:08 -0500

    refactor(organization.module)

--- src/organization/organization.module.ts
+++ src/organization/organization.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { organizationController } from './organization.controller';
 import { organizationService } from './organization.service';
+import { OrganizationRepository } from './organization.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([OrganizationRepository]),
+  ],
   controllers: [organizationController],
   providers: [organizationService]
 })
