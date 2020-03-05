c83605512f99070f791168c0352880e98d571b31
CarlosOrtiz9901 <carlos.ortiz@udla.edu.co>
2020-03-05 11:21:47 -0500

    fix(env): add environment variable

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { IndicadoresModule } from "./indicadores/indicadores.module";
-import ConfigService from "./common/config/config.service";
 import { UserModule } from "./user/userlogin.module";
 import { AuthModule } from './auth/auth.module';
 import { ProducersModule } from './producers/producers.module';
@@ -14,6 +13,7 @@ import { OrganizationModule } from './organization/organization.module';
 import { CadenasProductivasModule } from './cadenas-productivas/cadenas-productivas.module';
 import { InfrastructuresModule } from './infrastructures/infrastructures.module';
 import { MunicipalityModule } from './municipality/municipality.module';
+import ConfigService from './common/config/config.service';
 
 @Module({
   imports: [
@@ -28,7 +28,7 @@ import { MunicipalityModule } from './municipality/municipality.module';
     OrganizationModule,
     CadenasProductivasModule,
     InfrastructuresModule,
-    MunicipalityModule
+    MunicipalityModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get('ConfigService').envConfig;
+
   app.enableCors();
-  await app.listen(3001);
+  /* await app.listen(3001); */
+
+  const port = process.env.PORT || configService.APP_PORT || '3001'
+
+  await app.listen(port, '0.0.0.0');
+  console.log(`Server running on ${configService.APP_HOST_SERVER}:${port}/`);
+
 }
 bootstrap();
