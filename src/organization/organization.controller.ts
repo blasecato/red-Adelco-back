8ad55225b8e665ff882d87d24ee0e42cda16195c
CarlosOrtiz9901 <carlos.ortiz@udla.edu.co>
2020-06-21 14:34:50 -0500

    feat(organization):add query for get producer with organizationes

--- src/organization/organization.controller.ts
+++ src/organization/organization.controller.ts
@@ -45,6 +45,11 @@ export class organizationController {
     return await this._organizationService.getByIdOrganization(idOrganization);
   }
 
+  @Get('get/producer-organization')
+  async getPrododucerOrganization() {
+    return await this._organizationService.getPrododucerOrganization();
+  }
+
   @Get('count/persons-organization')
   async countPersonsOrganization(@Query('id') idOrganization: number) {
     return await this._organizationService.countPersonsOrganization(idOrganization);
